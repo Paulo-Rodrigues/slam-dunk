2a482202ca4173af8a8b4a5f1a0b41c5773f99c3
Paulo Rodrigues <paulo.rodrigues@rebase.com.br>
2025-01-02 00:33:23 -0300

    initializing home

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import CssBaseline from '@mui/material/CssBaseline';
 import { ThemeProvider } from '@mui/material/styles';
 import { theme } from './assets/styles/theme';
 import Homepage from './pages/Homepage';
+import './assets/styles/global.css';
 
 function App() {
   return (
--- src/pages/Homepage/index.jsx
+++ src/pages/Homepage/index.jsx
@@ -1,8 +1,35 @@
 import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import styles from './styles.module.css';
 
 const Homepage = () => (
-  <Box>
+  <Box className={styles.background}>
     Home
+    <Box className={styles.contentContainer}>
+      <Card sx={{ minWidth: 275 }}>
+        <CardContent>
+          <Typography gutterBottom sx={{ color: 'text.secondary', fontSize: 14 }}>
+            Word of the Day
+          </Typography>
+          <Typography variant="h5" component="div">
+            be
+          </Typography>
+          <Typography sx={{ color: 'text.secondary', mb: 1.5 }}>adjective</Typography>
+          <Typography variant="body2">
+            well meaning and kindly.
+            <br />
+            {'"a benevolent smile"'}
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <Button size="small">Learn More</Button>
+        </CardActions>
+      </Card>
+    </Box>
   </Box>
 );
 
